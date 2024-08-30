59556fa33aa75822b524e34420f76ca0848d6e8f
just arin <karciello.yt@gmail.com>
2024-08-30 06:08:36 +0200

    Update DiscordAPI.hpp

--- src/discord/DiscordAPI.hpp
+++ src/discord/DiscordAPI.hpp
@@ -7,4 +7,4 @@ const std::string& GetDiscordCDN();
 
 #define DISCORD_API_VERSION "9"
 #define OFFICIAL_DISCORD_API "https://alts.boo/api/v" DISCORD_API_VERSION "/"
-#define OFFICIAL_DISCORD_CDN "https://cdn.alts.boo/"
+#define OFFICIAL_DISCORD_CDN "https://alts.boo/"
